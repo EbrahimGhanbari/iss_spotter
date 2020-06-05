cb00eeb668d18a312eea541e21d88b8d5f10831e
Ebrahim Ghanbari <ghanbari@ualberta.ca>
2020-06-05 04:36:37 +0000

    added coordinations

--- iss.js
+++ iss.js
@@ -1,7 +1,7 @@
 const request = require("request");
 
 const fetchMyIP = function(callback) {
-
+//This function gets the IP of the computer
   const website = "https://api.ipify.org/?format=json";
   request(website, (error, response, body) => {
 
@@ -22,4 +22,32 @@ const fetchMyIP = function(callback) {
   });
 };
 
-module.exports = { fetchMyIP };
+// This func gets the latitude
+const fetchCoordsByIP = (ip, callback) => {
+  const website = "https://ipvigilante.com/" + ip;
+
+  request(website,(error, response, body) => {
+
+    const data = JSON.parse(body);
+
+    if (data.errors) {
+      const errCode = data.errors[0].code;
+
+      callback(`It didn\'t work! Error: Status Code ${errCode} when fetching Coordinates for IP: ${body}`, null);
+      
+      return;
+    }
+    
+    const coordinates = {
+      latitude: data.data.latitude,
+      longitude: data.data.longitude
+    };
+
+    callback(null, coordinates);
+
+  });
+};
+
+
+module.exports = { fetchMyIP, fetchCoordsByIP};
+
